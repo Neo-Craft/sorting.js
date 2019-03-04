f48b866815ef6e007e7e2c56004d1858c6f76117
Neo-Craft <sharp-man@live.fr>
2019-03-03 18:55:05 -0800

    Add merge & tim sort algorithms

--- sort.js
+++ sort.js
@@ -2,6 +2,14 @@ exports.quickSort = function(array) {
   return quickSort(array, 0, array.length - 1);
 }
 
+exports.mergeSort = function(array) {
+  return mergeSort(array, 0, array.length - 1);
+}
+
+exports.timSort = function(array, run = 2) {
+  return timSort(array, run);
+}
+
 //Quicksort a based sorting algorithm  on the divide-and-conquer approach.
 var quickSort = function(array, low, high){
   if (low < high){
@@ -33,4 +41,88 @@ var partition = function(array, low, high){
     [array[i], array[j]] = [array[j], array[i]];   //undo last swap when i ≥ j
   }
 
+}
+
+//Mergesort another based sorting algorithm on the divide-and-conquer approach.
+var mergeSort = function(array, low, high){
+  if (low < high){
+    var middle = low + Math.floor((high - low) / 2);
+    mergeSort(array, low, middle);                //sort first half
+    mergeSort(array, middle + 1, high);           //sort second half
+
+    mergeSortedArrays(array, low, middle, high);
+  }
+};
+
+
+var mergeSortedArrays = function(array, low, middle, high){
+  if (low >= array.length || high >= array.length){
+    return;
+  }
+  //Pivot for Partition
+  var left = low, right = middle + 1;
+  var temp = [];
+
+  while (left <= middle && right <= high){
+    if (array[left] < array[right]){
+      temp.push(array[left++]);
+    }
+    else if (array[left] > array[right]){
+      temp.push(array[right++]);
+    }
+    else {
+      temp.push(array[left++]);
+      temp.push(array[right++]);
+    }
+  }
+
+  while (left <= middle){
+    temp.push(array[left++]);
+  }
+
+  while (right <= high){
+    temp.push(array[right++]);
+  }
+
+  for(var i = low; i <= high; i++){
+    array[i] = temp[i - low];
+  }
+}
+
+
+//TimeSort, another based sorting algorithm on the divide-and-conquer approach.
+//For this algorithm we will divide the array into x subarrays, sort each subarray then use the previous mergeSortedArrays() function to merge subarrays
+var timSort = function(array, run){
+  for(var i = 0; i < array.length; i += run){
+    insertionSort(array, i, Math.max(array.length - 1, i + run));      //if array size doesn't divide run evenly then takes last parameters
+  }
+  var bound = run;
+  while(bound < array.length){
+    for(var left = 0; left < array.length; left += 2*bound){
+      //starting point of sub array
+      var mid = left + bound - 1;
+      //ending point of left sub array
+      var right = Math.min((left + 2*bound - 1), (array.length - 1));
+      //merge arr[left.....mid] & arr[mid+1....right]
+      mergeSortedArrays(array, left, mid, right);
+    }
+
+    bound *= 2;
+  }
+};
+
+//an O(n^2) sorting algoritim used in the divide-and-conquer approach
+var insertionSort = function(array, low, high){
+  if (low >= array.length || high >= array.length){
+    return;
+  }
+
+  for(var index = 1; index <= high; index++){
+    var temp = array[index];
+    var index2 = index - 1;
+    for(; index2 >= 0 && array[index2] > temp; index2--){
+        array[index2 + 1] = array[index2];
+    }
+    array[index2 + 1] = temp;
+  }
 }
--- test/timsort_test.js
+++ test/timsort_test.js
@@ -2,22 +2,35 @@ var assert = require('assert');
 var klass = require('../sort.js');
 
 describe('Log(n) sorting', function() {
-  describe('#quickSort()', function() {
+  describe('#timSort()', function() {
       it('should sort a data set as expected', function() {
         var data = [23,24,2,342,4,324,3,12,1];
-        klass.quickSort(data);
+        klass.timSort(data);
+        assert.deepEqual(data, [1,2,3,4,12,23,24,324,342]);
+        var data = [23,24,2,342,4,324,3,12,1];
+        klass.timSort(data, 4); //different run
         assert.deepEqual(data, [1,2,3,4,12,23,24,324,342]);
     });
   });
-  describe('#quickSort()', function() {
+  describe('#timSort()', function() {
       it('should sort negative & repetitive values', function() {
         var data = [1,2,-1,-2,0,3,-0,2];
-        klass.quickSort(data);
+        klass.timSort(data);
         assert.deepEqual(data, [-2,-1,-0,0,1,2,2,3]);
 
         var neg_data = [2,0,-0,2];
-        klass.quickSort(neg_data);
+        klass.timSort(neg_data);
         assert.deepEqual(neg_data, [-0,0,2,2]);
+
+
+    });
+  });
+
+  describe('#timSort()', function() {
+      it('should sort odd array size', function() {
+        var neg_data = [2,2,-2];
+        klass.timSort(neg_data);
+        assert.deepEqual(neg_data, [-2,2,2]);
     });
   });
 });
